a573ed886cb0e44fe40fc6de134db92bedec8214
ANTOMARCHI Mallory <mantomarchi@VILLEFRANCHE.local>
2023-12-18 16:29:39 +0100

    test

--- index.js
+++ index.js
@@ -31,14 +31,13 @@ onValue(shoppingListInDB, function(snapshot) {
         
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
             
             appendItemToShoppingListEl(currentItem)
         }    
     } else {
         shoppingListEl.innerHTML = "Aucune tâche pour le moment"
     }
+
 })
 
 function clearShoppingListEl() {
@@ -52,15 +51,17 @@ function clearInputFieldEl() {
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
+    let isClicked = false
     
     let newEl = document.createElement("li")
+    newEl.setAttribute("id", "blank")
     
     newEl.textContent = itemValue
 
-    newEl.addEventListener("click", function() {
-        newEl.style.backgroundColor = "green"
-        newEl.style.color = "white"
+    newEl.addEventListener("click", () => {
+        return newEl.setAttribute("id", "clicked")
     })
+
     
     newEl.addEventListener("dblclick", function() {
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
@@ -70,3 +71,4 @@ function appendItemToShoppingListEl(item) {
     
     shoppingListEl.append(newEl)
 }
+
